37381dcd1013575eb8aeca0a699b62b178f03f73
PhurinGZ <120617446+PhurinGZ@users.noreply.github.com>
2024-11-15 12:20:42 +0700

    fix category authOption

--- web-novel/src/app/api/categories/[id]/route.tsx
+++ web-novel/src/app/api/categories/[id]/route.tsx
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
 import Category from "@/models/Category";
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
 
 export async function GET(
   req: Request,
