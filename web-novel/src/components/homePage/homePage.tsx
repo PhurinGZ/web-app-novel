db19c887fadac4f4d981a6043714657c5364296e
Phurin Gannedgad <BasPhurin2502@gmail.com>
2024-11-15 19:56:20 +0700

    Update homePage.tsx

--- web-novel/src/components/homePage/homePage.tsx
+++ web-novel/src/components/homePage/homePage.tsx
@@ -7,16 +7,16 @@ import SliderBook from "../sliderbook/sliderBook";
 import Footer from "../footer/footer";
 import useSWR from "swr";
 import Loading from "@/components/loading/loading";
+import { useSession } from "next-auth/react";
 
 function HomePage() {
   const [dataNovel, setDataNovel] = useState(null);
+  const { data: session, status } = useSession();
 
-  const fetcher = (url: string | Request | URL) => fetch(url).then((res) => res.json());
+  const fetcher = (url: string | Request | URL) =>
+    fetch(url).then((res) => res.json());
 
-  const { data: novelData, error: novelError } = useSWR(
-    "/api/novels",
-    fetcher
-  );
+  const { data: novelData, error: novelError } = useSWR("/api/novels", fetcher);
 
   // console.log(novelData)
 
@@ -35,7 +35,10 @@ function HomePage() {
 
   if (listNovelsError)
     return <div>Failed to load, Error : {listNovelsError.message}</div>;
+
+  if (!session) return <Loading />;
   if (!listNovelsData) return <Loading />;
+  if (!novelData) return <Loading />;
 
   // console.log(dataNovel);
 
@@ -50,14 +53,48 @@ function HomePage() {
             <Searchbar />
           </div> */}
           <div className="content-home">
-            {listNovelsData?.map((l: { nameListTH: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; novels: { id: number; name: string; desc: string; category: { name: String; }; rate: { name: String; }; rating: number[]; detail: string; tag: string; chapter: { id: number; name: string; content: string; }[]; createdBy: { username: String; }; public: boolean; updateAt: string; createAt: string; }[]; }, index: React.Key | null | undefined) => (
-              <div key={index} className="content">
-                <h1 className="text-3xl font-bold mb-4">{l.nameListTH}</h1>
-                <div className="book-cover-home-page">
-                  <SliderBook dataCardNovel={l.novels} />
+            {listNovelsData?.map(
+              (
+                l: {
+                  nameListTH:
+                    | string
+                    | number
+                    | boolean
+                    | React.ReactElement<
+                        any,
+                        string | React.JSXElementConstructor<any>
+                      >
+                    | Iterable<React.ReactNode>
+                    | React.ReactPortal
+                    | Promise<React.AwaitedReactNode>
+                    | null
+                    | undefined;
+                  novels: {
+                    id: number;
+                    name: string;
+                    desc: string;
+                    category: { name: String };
+                    rate: { name: String };
+                    rating: number[];
+                    detail: string;
+                    tag: string;
+                    chapter: { id: number; name: string; content: string }[];
+                    createdBy: { username: String };
+                    public: boolean;
+                    updateAt: string;
+                    createAt: string;
+                  }[];
+                },
+                index: React.Key | null | undefined
+              ) => (
+                <div key={index} className="content">
+                  <h1 className="text-3xl font-bold mb-4">{l.nameListTH}</h1>
+                  <div className="book-cover-home-page">
+                    <SliderBook dataCardNovel={l.novels} />
+                  </div>
                 </div>
-              </div>
-            ))}
+              )
+            )}
 
             <div className="content">
               <h1 className="text-3xl font-bold mb-4">นิยายทั้งหมด</h1>
